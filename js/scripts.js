9e63a50b317eb1ebcb369c4ab231f5a72c3a4253
cassio <cassiocarvalho49@live.com>
2021-12-07 15:41:35 -0300

    add js ao index

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function clicar() {
+  var a = document.getElementById('mudar')
+  a.innerHTML = 'PRODUTO INDISPONIVEL!'  
+}
+function mudar() {
+  var a = document.getElementById('alterar')
+  a.innerHTML = 'SEM ESTOQUE!'  
+}
+function aperte() {
+  var a = document.getElementById('indisponivel')
+  a.innerHTML = 'INDISPONIVEL!'  
+}
+function ap() {
+  var a = document.getElementById('ap')
+  a.innerHTML = 'QUEBRADO!'  
+}
+function a() {
+  var a = document.getElementById('a')
+  a.innerHTML = 'PRODUTO INDISPONIVEL!'  
+}
+function b() {
+  var a = document.getElementById('b')
+  a.innerHTML = 'PRODUTO INDISPONIVEL!'  
+}
+function c() {
+  var a = document.getElementById('c')
+  a.innerHTML = 'PRODUTO INDISPONIVEL!'  
+}
+function d() {
+  var a = document.getElementById('d')
+  a.innerHTML = 'PRODUTO INDISPONIVEL!'  
+}
